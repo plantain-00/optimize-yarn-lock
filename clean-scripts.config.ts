26f6069130ee41e260244693c074ba9f826aa43a
York Yao <me@yorkyao.com>
2020-06-26 10:57:41 +0800

    chore: remove commitlint

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -1,7 +1,6 @@
 import { checkGitStatus } from 'clean-scripts'
 
 const tsFiles = `"src/**/*.ts"`
-const jsFiles = `"*.config.js"`
 
 export default {
   build: [
@@ -10,9 +9,8 @@ export default {
     'node dist/index.js -p ./spec/yarn.lock --check --supressError > spec/result.txt'
   ],
   lint: {
-    ts: `eslint --ext .js,.ts ${tsFiles} ${jsFiles}`,
+    ts: `eslint --ext .js,.ts ${tsFiles}`,
     export: `no-unused-export ${tsFiles} --strict --need-module tslib`,
-    commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'type-coverage -p src --strict'
   },
@@ -20,5 +18,5 @@ export default {
     'clean-release --config clean-run.config.ts',
     () => checkGitStatus()
   ],
-  fix: `eslint --ext .js,.ts ${tsFiles} ${jsFiles} --fix`
+  fix: `eslint --ext .js,.ts ${tsFiles} --fix`
 }
